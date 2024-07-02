0f13bdb3547087e6e14d225dabc0eef911894dae
JoseSelmar <selmar.unimedbv@gmail.com>
2024-07-02 16:49:50 -0300

    findIdSeller implements

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,7 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -12,12 +9,9 @@ public class Program {
 	public static void main(String[] args) {
 		
 
-		Department d = new Department(1,"teste");
-		
-		Seller s = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.0,d);
 		
 		SellerDao sD = DaoFactory.createSellerDao();
-		
+		Seller s = sD.findById(3);
 		
 		System.out.println(s);
 
