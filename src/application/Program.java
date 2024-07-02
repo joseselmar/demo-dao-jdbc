a4261abc34b26353a73488de5c97b532a5b015f6
JoseSelmar <selmar.unimedbv@gmail.com>
2024-07-02 14:57:21 -0300

    seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+
 		Department d = new Department(1,"teste");
 		
-		System.out.println(d);
+		Seller s = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.0,d);
+		
+		System.out.println(s);
 
 	}
 
